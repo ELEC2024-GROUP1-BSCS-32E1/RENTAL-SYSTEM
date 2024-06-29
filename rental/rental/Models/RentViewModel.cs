97923226583b0236c8406b1fe63ad594119f2029
king-paul-bscs31a1 <161002110+king-paul-bscs31a1@users.noreply.github.com>
2024-06-29 17:51:35 +0800

    new rental converted to mvc

---
+++ rental/rental/Models/RentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace rental.Models
+{
+    public class RentViewModel
+    {
+        public int EquipmentId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string ImageUrl { get; set; } // Add ImageUrl property
+        public string Price { get; set; }    // Add Price property
+    }
+}
